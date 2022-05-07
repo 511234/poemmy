d8c6b739b3b9efe852e9540f6a81b339a8c0f3be
511234 <tungkl1200@gmail.com>
2022-05-07 16:57:36 +0800

    📦 NEW: Allow refresh poem with last fired request

--- src/components/Poem/Poem.tsx
+++ src/components/Poem/Poem.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from "react"
 import { iPoem } from "../../typescript/interfaces/poem"
 import { POEM_ENDPOINT } from "../../utilities/constants"
 import "./poem.sass"
-import { of, map, fromEvent, switchMap } from "rxjs"
+import { of, fromEvent, switchMap, catchError } from "rxjs"
 import { fromFetch } from "rxjs/fetch"
 
 export const Poem = () => {
@@ -19,9 +19,39 @@ export const Poem = () => {
     const { title, author, linecount, lines } = poem
 
     useEffect(() => {
-        const getPoem = fromFetch(POEM_ENDPOINT)
-            .pipe(switchMap((response: any) => response.json()))
+        const getPoem$ = fromFetch(POEM_ENDPOINT).pipe(
+            switchMap((response) => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    // Server is returning a status requiring the client to try something else.
+                    return of({ error: true, message: `Error ${response.status}` })
+                }
+            }),
+            catchError((err) => {
+                // Network or other error, handle appropriately
+                console.error(err)
+                return of({ error: true, message: err.message })
+            })
+        )
+
+        const getPoem = () => {
+            return getPoem$
+        }
+
+        getPoem$.subscribe({
+            next: (result) => {
+                console.log(result)
+                setPoem(result?.[0])
+            },
+            complete: () => console.log("Request completed"),
+        })
+
+        const refreshPoem = fromEvent(buttonRef.current, "click")
+            .pipe(switchMap(getPoem))
             .subscribe((res: any) => res.then(setPoem(res?.[0])))
+
+        return () => refreshPoem.unsubscribe()
 
         // const getPoem = fromFetch(POEM_ENDPOINT).pipe(switchMap((response: any) => response.json()))
 
@@ -43,8 +73,12 @@ export const Poem = () => {
                     <div className="poem-title">{title} </div>
                     <div className="poem-author">{author} </div>
                 </div>
-                {lines.map((line) => {
-                    return <div className="poem-line">{line}</div>
+                {lines.map((line, index) => {
+                    return (
+                        <div className="poem-line" key={index}>
+                            {line}
+                        </div>
+                    )
                 })}
             </div>
             <button ref={buttonRef} className="poem-regenerate-button">
