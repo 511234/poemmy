02e7ca86176fb136571e335844304d3028c4a077
511234 <tungkl1200@gmail.com>
2022-05-07 16:39:31 +0800

    📦 NEW: Fetch poem with rxjs

--- src/components/Poem/Poem.tsx
+++ src/components/Poem/Poem.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useRef, useState } from "react"
 import { iPoem } from "../../typescript/interfaces/poem"
 import { POEM_ENDPOINT } from "../../utilities/constants"
 import "./poem.sass"
+import { of, map, fromEvent, switchMap } from "rxjs"
+import { fromFetch } from "rxjs/fetch"
 
 export const Poem = () => {
     const poemInitialState = {
@@ -12,18 +14,27 @@ export const Poem = () => {
         lines: [""],
     }
 
-    const buttonRef = useRef(null);
+    const buttonRef = useRef({} as HTMLButtonElement)
     const [poem, setPoem] = useState<iPoem>(poemInitialState)
     const { title, author, linecount, lines } = poem
 
     useEffect(() => {
-        getPoem()
+        const getPoem = fromFetch(POEM_ENDPOINT)
+            .pipe(switchMap((response: any) => response.json()))
+            .subscribe((res: any) => res.then(setPoem(res?.[0])))
+
+        // const getPoem = fromFetch(POEM_ENDPOINT).pipe(switchMap((response: any) => response.json()))
+
+        // const observer = fromEvent(buttonRef.current, "click")
+        //     .pipe(switchMap(getPoem))
+        //     .subscribe((value) => console.log(value))
+        // observer.unsubscribe()
     }, [])
 
-    const getPoem = async () => {
-        const response = await axios.get(POEM_ENDPOINT)
-        setPoem(response.data?.[0])
-    }
+    // const getPoem = async () => {
+    //     const response = await axios.get(POEM_ENDPOINT)
+    //     setPoem(response.data?.[0])
+    // }
 
     return (
         <>
@@ -36,7 +47,7 @@ export const Poem = () => {
                     return <div className="poem-line">{line}</div>
                 })}
             </div>
-            <button ref={buttonRef} onClick={getPoem} className="poem-regenerate-button">
+            <button ref={buttonRef} className="poem-regenerate-button">
                 Get New Poem
             </button>
         </>
